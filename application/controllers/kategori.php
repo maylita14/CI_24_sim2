408dd0623be4ad482c992ddfd27f94dee991be8f
maylita14 <maylitasumarnah@gmail.com>
2026-03-03 14:31:29 +0700

    pertemuan3

--- application/controllers/kategori.php
+++ application/controllers/kategori.php
@@ -15,4 +15,17 @@ class kategori extends CI_Controller{
         $this->load->view('kategori/index', $data);
 
     }
+    public function tambah()
+    {
+        $this->load->view('kategori/tambah');
+    }
+    public function simpan()
+    {
+        $data= [
+            'nama_kategori'=> $this->input->post('nama_kategori')
+        ];
+
+    $this->Kategori_model->insert($data);
+    redirect('kategori');
+    }
 }
